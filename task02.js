87b2227fe565ee827d797915d31701fa985e2822
setthawut-kao <setthawut.kao@gmail.com>
2025-07-16 09:27:22 +0700

    task02 solutions

--- task02.js
+++ task02.js
@@ -15,6 +15,22 @@ Task 02: Check Even or Odd
 
 // 👨‍💻 TODO-01: Write your code below to complete the task as described above.
 
+// Solution 1 : Function Expression
+// const checkEvenOrOdd = function(number) {
+//     if (number % 2 === 0) {
+//         return "Even";
+//     } else {
+//         return "Odd";
+//     }
+// };
+// Solution 2 : Arrow Function
+// const checkEvenOrOdd = (number) => {
+//     return number % 2 === 0 ? "Even" : "Odd";  // ternary operation
+// };
+// Solution 3
+const checkEvenOrOdd = (number) => number % 2 === 0 ? "Even" : "Odd";  // ternary operation
+;
+
 // Example Test Cases
 console.log(checkEvenOrOdd(4)); // "Even"
 console.log(checkEvenOrOdd(9)); // "Odd"
