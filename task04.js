bf9d3e5cfe71ce5ae5265ea1413523ea0b41d0b1
setthawut-kao <setthawut.kao@gmail.com>
2025-07-16 09:50:22 +0700

    task04 solutions

--- task04.js
+++ task04.js
@@ -18,6 +18,37 @@ Task 04: Traffic Light Action
 
 // 👨‍💻 TODO-01: Write your code below to complete the task as described above.
 
+// function trafficLightAction(color) {
+//   switch (color) {
+//     case "green":
+//       return "Go";
+//     case "yellow":
+//       return "Slow down";
+//     case "red":
+//       return "Stop";
+//     default:
+//       return "Invalid light color";
+//   }
+// }
+
+function trafficLightAction(color) {
+  let message = "";
+  switch (color) {
+    case "green":
+      message = "Go";
+      break;
+    case "yellow":
+      message = "Slow down";
+      break;
+    case "red":
+      message = "Stop";
+      break;
+    default:
+      message = "Invalid light color";
+  }
+  return message;
+}
+
 // Example Test Cases
 console.log(trafficLightAction("green")); // "Go"
 console.log(trafficLightAction("yellow")); // "Slow down"
